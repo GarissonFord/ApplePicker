70c86ca6cdfef165d27c197b00ffb67a9e8a07f8
Gary DiCiano <garydiciano4@gmail.com>
2017-06-14 19:05:52 -0400

    High Score Key added
    Now there’s a consistent high score stored on my computer if I wanted
    to play this again

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+	static public int score = 1000;
+	public Text highScoreGT;
+
+	void Awake()
+	{
+		//Reads score if it already exists
+		if (PlayerPrefs.HasKey ("ApplePickerHighScore"))
+			score = PlayerPrefs.GetInt ("ApplePickerHighScore");
+
+		//Assigns high score to ApplePickerHighScore
+		PlayerPrefs.SetInt("ApplePickerHighScore", score);
+	}
+
+	void Update()
+	{
+		GameObject highScoreGO = GameObject.Find ("HighScore");
+		highScoreGT = highScoreGO.GetComponent<Text> ();
+		highScoreGT.text = "High Score: " + score;
+
+		//Can update ApplePickerHighScore in PlayerPrefs
+		if (score > PlayerPrefs.GetInt ("ApplePickerHighScore"))
+			PlayerPrefs.SetInt ("ApplePickerHighScore", score);
+	}
+}
