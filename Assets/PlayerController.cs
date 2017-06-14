7ce2c29413e2b3068f19fea7e4aeb037510bbe6a
Gary DiCiano <garydiciano4@gmail.com>
2017-06-13 20:55:57 -0400

    Revert "Player Movement Started"
    This reverts commit c94a2efb266f347ee913690a476e34479ad70729.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () 
-	{
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () 
-	{
-		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+	void Update () {
+		
 	}
 }
