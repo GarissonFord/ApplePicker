ee239d6363cbd8e6ffecfb8bd7320dbd8b24b85a
Gary DiCiano <garydiciano4@gmail.com>
2017-06-13 22:37:25 -0400

    Basic Tree Movement

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class AppleTree : MonoBehaviour {
 
+	public GameObject applePrefab;
+
+	public float speed = 1f;
+
+	//Reference to how far left and right tree can move 
+	public float leftAndRightEdge = 10f;
+
+	public float chanceToChangeDirections = 0.1f;
+
+	public float secondsBetweenAppleDrops = 1f;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		//Movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		//Change direction
+		if (pos.x < -leftAndRightEdge) 
+		{
+			speed = Mathf.Abs (speed); //Moves right
+		}
+		else if(pos.x > leftAndRightEdge)
+		{
+			speed = -Mathf.Abs (speed); //Moves left
+		}
 	}
 }
