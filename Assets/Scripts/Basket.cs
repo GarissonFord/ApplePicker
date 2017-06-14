5e0c23efe9762885ad73bf8c0e7de90d5e3f3bfb
Gary DiCiano <garydiciano4@gmail.com>
2017-06-13 23:06:29 -0400

    Created Player
    Can also destroy Apple prefabs on colliding with them

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Basket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		//Current screen position of mouse
+		Vector3 mousePos2D = Input.mousePosition;
+		//Because camera's Z position is -10, we need the reverse to push the mouse
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+		//Move x position of basket to mouse x position
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.CompareTag ("Apple"))
+			Destroy (collidedWith);
 	}
 }
