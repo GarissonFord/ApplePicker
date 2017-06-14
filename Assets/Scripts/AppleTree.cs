9fa90b67c47b855a1e86f86f8424ae2837fc45c0
Gary DiCiano <garydiciano4@gmail.com>
2017-06-13 20:56:13 -0400

    Revert "Apples fall at consistent rate"
    This reverts commit 0ea06c226a1cfbb73d979711590167164dd328eb.

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -5,21 +5,14 @@ public class AppleTree : MonoBehaviour {
 
 	public GameObject apple;
 	//Time in seconds until next apple
-	public float appleRate;
-	private float nextApple;
+	public float nextAppleTime;
 	// Use this for initialization
 
 	// Update is called once per frame
 	void Update () 
 	{
-
-		if (Time.time > nextApple)
-		{
-			nextApple = Time.time + appleRate;
-
-			Vector3 position = transform.position;
-			Quaternion rotation = transform.rotation;
-			Instantiate (apple, position, rotation);
-		}
+		Vector3 position = transform.position;
+		Quaternion rotation = transform.rotation;
+		Instantiate (apple, position, rotation);
 	}
 }
